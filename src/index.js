0286b3ee4bd0fd24fa90ca183eb02f89eb09e0b0
yktk435 <yokotsuka435@gmail.com>
2020-09-29 23:37:58 +0900

    サンプルコード

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
+import { Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import createBrowserHistory from 'history/createBrowserHistory';
+import TodoApp from './containers/TodoApp';
+import Error from './components/Error';
+import createStore from './store';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+// historyのインスタンスを生成
+const history = createBrowserHistory();
+
+// Storeの生成
+const store = createStore(history);
+
+render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        {/* ルーティングさせる */}
+        <Route exact path="/" component={TodoApp} />
+        <Route path="/error" component={Error} />
+      </div>
+    </ConnectedRouter>
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
